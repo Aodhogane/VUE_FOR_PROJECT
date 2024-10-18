e8277fedaf4420ca1a56b4e2b28a8b8ecc92364a
Илья <152794862+Aodhogane@users.noreply.github.com>
2024-10-19 01:28:22 +0300

    -

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,56 +1,77 @@
 <template>
-    <div class="card">
-      <img :src="starImage" alt="Звезда" class="star-icon" />
-      <h2>{{ title }}</h2>
-      <p>{{ price }}₽</p>
-      <p>{{ location }}</p>
-      <button class="rent-button">Арендовать</button>
-    </div>
-  </template>
-  
-  <script>
-  import starImage from '../assets/Star_1.png'; // Путь к изображению
-  
-  export default {
-    name: 'Card',
-    props: {
-      title: {
-        type: String,
-        required: true,
-      },
-      price: {
-        type: Number,
-        required: true,
-      },
-      location: {
-        type: String,
-        required: true,
-      },
+  <div class="card" @click="goToProductPage">
+    <img :src="starImage" alt="Звезда" class="star-icon" />
+    <h2>{{ title }}</h2>
+    <p>{{ price }}₽</p>
+    <p>{{ location }}</p>
+    <button class="rent-button" @click="goToProductPage">Арендовать</button>
+  </div>
+</template>
+
+<script>
+import starImage from '../assets/Star_1.png';
+
+export default {
+  name: 'Card',
+  props: {
+    id: {
+      type: Number,
+      required: true,
     },
-    data() {
-      return {
-        starImage, // Сохраняем изображение в data
-      };
+    title: {
+      type: String,
+      required: true,
     },
-  };
-  </script>
-  
-  <style>
-  .card {
-    border: 1px solid #ccc;
-    border-radius: 8px;
-    padding: 10px;
-    width: 200px;
-    text-align: center;
-    position: relative;
-  }
-  
-  .star-icon {
-    width: 24px; /* Измените размер по необходимости */
-    position: absolute;
-    top: 10px; /* Измените по необходимости для позиционирования */
-    right: 10px; /* Измените по необходимости для позиционирования */
-    animation: bounce 1s infinite; /* Пример анимации */
-  }
+    price: {
+      type: Number,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      starImage,
+    };
+  },
+  methods: {
+    goToProductPage() {
+      this.$router.push({ name: 'ProductDetail', params: { id: this.id } });
+    },
+  },
+};
+</script>
+
+<style>
+.card {
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  padding: 10px;
+  width: 200px;
+  text-align: center;
+  position: relative;
+}
+
+.star-icon {
+  width: 24px;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  animation: bounce 1s infinite; /* Пример анимации */
+}
+
+.rent-button {
+  background-color: #ff4747; /* Цвет кнопки */
+  color: white;
+  border: none;
+  padding: 8px 12px;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.rent-button:hover {
+  background-color: #e04040; /* Цвет при наведении */
+}
 </style>
-  
