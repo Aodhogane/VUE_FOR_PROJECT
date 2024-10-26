670ac3d8d85f82f6cf74a831f651a8db424b6945
Илья <152794862+Aodhogane@users.noreply.github.com>
2024-10-26 17:15:06 +0300

    Избавились от ненужныйх файлов

--- src/dist/router.dev.js
+++ src/dist/router.dev.js
@@ -9,24 +9,12 @@ var _vueRouter = require("vue-router");
 
 var _Home = _interopRequireDefault(require("./components/Home.vue"));
 
-var _About = _interopRequireDefault(require("./components/About.vue"));
-
-var _Contact = _interopRequireDefault(require("./components/Contact.vue"));
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var routes = [{
   path: '/',
   name: 'Home',
   component: _Home["default"]
-}, {
-  path: '/about',
-  name: 'About',
-  component: _About["default"]
-}, {
-  path: '/contact',
-  name: 'Contact',
-  component: _Contact["default"]
 }];
 var router = (0, _vueRouter.createRouter)({
   history: (0, _vueRouter.createWebHistory)(),
--- src/router.js
+++ src/router.js
@@ -1,23 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './components/Home.vue';
-import About from './components/About.vue';
-import Contact from './components/Contact.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
-  },
-  {
-    path: '/contact',
-    name: 'Contact',
-    component: Contact
   }
 ];
 
