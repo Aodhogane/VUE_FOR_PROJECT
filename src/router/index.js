1cec5c15017019671c2e920ef9988b8cba2200f2
Илья <152794862+Aodhogane@users.noreply.github.com>
2024-10-28 22:02:55 +0300

    Удаления ненужного.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '@/components/Home.vue';
-import ProductDetail from '@/components/ProductDetail.vue';
+import Home from '../components/Home.vue';
+import ProductDetail from '../components/ProductCard.vue';
 
 const routes = [
   {
--- src/utils/filters.js
+++
@@ -1,9 +0,0 @@
-export function filterProducts(products, searchQuery, selectedCategory, priceRange) {
-    return products.filter(product =>
-      (!selectedCategory || product.category === selectedCategory) &&
-      product.price >= priceRange.min &&
-      product.price <= priceRange.max &&
-      product.name.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-  }
-  
